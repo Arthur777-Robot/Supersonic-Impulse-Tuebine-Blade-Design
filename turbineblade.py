21397fcc5ada00e6d4c1c6d2ab04551a9864f844
Arthur <albinosnake777@gmail.com>
2017-04-11 20:56:43 +0900

    get_R function updated

--- turbineblade.py
+++ turbineblade.py
@@ -38,6 +38,43 @@ class Blade():
 	def chara_y(self,Rstar,theta):
 		return Rstar*math.cos(theta) 
 
+	# draws 2 characteristic lines. the angle starts from v1(compression wave) and v2(expansion wave).
+	def draw_lines(self,v1,v2):
+		
+		v1 = math.radians(v1)
+		v2 = math.radians(v2)
+
+		counter = 1000
+		x0,y0,x1,y1,R_x,R_y,R_x_min,R_y_min = [],[],[],[],[],[],[],[]
+		for num in range(1,counter):
+			Rstar = self.Rstar_min + num*1/counter 
+			if Rstar > 1: 
+				Rstar = 1
+			theta1 = (self.chara_line(Rstar) - self.const) + v1
+			theta2 = -(self.chara_line(Rstar) - self.const) + v2 
+			x0 += [(self.chara_x(Rstar,theta1))]
+			y0 += [(self.chara_y(Rstar,theta1))]
+			x1 += [(self.chara_x(Rstar,theta2))]
+			y1 += [(self.chara_y(Rstar,theta2))]
+
+			if Rstar == 1: break
+
+		for num in range(0,360):
+			R_x += [-math.sin(math.radians(num))]
+			R_y += [math.cos(math.radians(num))]
+			R_x_min += [-math.sin(math.radians(num))*self.Rstar_min]
+			R_y_min += [math.cos(math.radians(num))*self.Rstar_min]
+
+		plt.plot(x0,y0)
+		plt.plot(x1,y1)
+		plt.plot(R_x,R_y)
+		plt.plot(R_x_min,R_y_min)
+		plt.xlim(-1,1)
+		plt.ylim(-1,1)
+		plt.gca().set_aspect('equal', adjustable='box')
+		plt.show()
+
+
 	def plot_chara(self,angle,step):
 
 		x1,y1,x2,y2 = [],[],[],[]
@@ -59,40 +96,48 @@ class Blade():
 
 			plt.plot(x1,y1,"r")
 			plt.plot(x2,y2,"k")
+		plt.xlim(-1,1)
+		plt.ylim(-1,1)
+		plt.gca().set_aspect('equal', adjustable='box')
 		plt.show()
 
-	def get_R(self,org,nyu):
-		
-		delta = math.radians(nyu)*2
-		org = math.radians(org)
-		Rstar = self.Rstar_min
+	# top arc angle is 0 
+	# v1 must be smaller than v2
+	def get_R(self,v1,v2):
+
+		v1 = math.radians(v1)
+		v2 = math.radians(v2)
+		Rstar = 1	#initial Rstar
 		param = []
 		temp_x = 1
 		temp_y = 1
+		myu_check = math.radians(90)
 
 		#this for is very stupid. intersecting angle is always half the delta
 		for num in range(1,10): #decimal precision of Rstar
 			while(1):
-				if Rstar >= 1:
-					Rstar = 1	
-				theta1 = self.chara_line(Rstar) - self.const + org
-				theta2 = theta1 - delta
-				x0 = self.chara_x(Rstar,-theta1)
-				y0 = self.chara_y(Rstar,-theta1)
+				theta1 = (self.chara_line(Rstar) - self.const) + v1
+				theta2 = -(self.chara_line(Rstar) - self.const) + v2 
+				x0 = self.chara_x(Rstar,theta1)
+				y0 = self.chara_y(Rstar,theta1)
 				x1 = self.chara_x(Rstar,theta2)
 				y1 = self.chara_y(Rstar,theta2)
-				if x0 - x1 > 0: 
+#				print(Rstar,math.degrees(theta1),math.degrees(theta2))
+				print(x0,y0,x1,y1,Rstar)
+				if x1 - x0 < 0: 
 					if(temp_x - x1 == 0):		#this if is to avoid my fucking bug
 						myu_check = math.radians(90)		#no meaning for the value. 
 					else:
 						myu_check = math.atan((temp_y - y1)/(temp_x - x1))
+						if myu_check == 0:
+							print(theta1,theta2)
 					temp_x = x1
 					temp_y = y1
-					Rstar = Rstar - 1/(10**num) 
+					Rstar = Rstar + 1/(10**num) 
 
 					break
 
-				Rstar = Rstar + 1/(10**num)
+				Rstar = Rstar - 1/(10**num)
 
 		param.append(Rstar)
 		param.append(x0)
@@ -109,101 +154,91 @@ class Blade():
 	def get_mach(self,Mstar):
 		return math.sqrt((2 * Mstar**2)/((self.gamma + 1) - (self.gamma - 1) * Mstar**2))
 
-	# v1 is the start angle,ve is end angle
-	def lower_concave(self,v1,ve):
+	def get_Ru(self,vu):
 
-		myu = math.radians(2)
-		fai = math.radians(4)
-		Rstar,Xstar_a,Ystar_a = self.get_R(0,2)
-		
-		b1 = math.tan(myu + fai)
-		c1 = -(Ystar_a + Xstar_a * math.tan(myu + fai))
-		b2 = -math.tan(-fai)
+		Rstar = 1 #initial Rstar
 
-		delta3 = b2 - b1
-		
-		f = delta3 + math.tan(-fai)
-		g = delta3 + b1
+		for num in range(1,10):
+			while(1):
+				theta = self.chara_line(Rstar) - self.const -  math.radians(vu)
+				x = self.chara_x(Rstar,theta)
+				y = self.chara_y(Rstar,theta)
+				if x > 0:
+					Rstar = Rstar + 1/(10**num) 
+					break
+				Rstar = Rstar - 1/(10**num)
+		print(Rstar,x,y)
+		return Rstar
 
-		print(b1,c1,b2,delta3,f,g)
-
-#		print(((c1 / f) - (c1 * b2) / (f * g)) ,  (b1 * math.tan(-fai)) , (f * g))
-#		Xstar_b = ((c1 / f) - (c1 * b2) / (f * g)) / (1 - (b1 * math.tan(-fai)) / (f * g))
-		Xstar_b = 0
-		Ystar_b = ((b1 * Xstar_b * math.tan(-fai)) - c1 * b1) / g
-
-		print(Xstar_b,Ystar_b)
-
-		
-	# vu is the start angle, Ve is the end angle
+	# defines upper convex arc coordinates
+	# ve is the entry angle
+	# vu is the value to define the upper convex radius
 	def upper_convex(self,vu,ve):
 
-		myu = math.radians(2)
-		fai = math.radians(4)
+		interval = math.radians(2)
+
+		Rustar = self.get_Ru(vu)
+		xinit = 0
+		yinit = Rustar
+
+		Xstar_b = xinit
+		Ystar_b = yinit
+
+		Rstar,Xstar_a,Ystar_a,myu_check = self.get_R(-vu,vu)
+#		myu = self.get_myu(self.get_mach(self.get_Mstar(Rstar)))
+#		a1 = math.tan(-myu+math.radians(num))
+#		b1 = Ystar_a - a1 * Xstar_a
+#		a2 = math.tan(math.radians(num))
+#		b2 = Ystar_b - a2 * Xstar_b
+
+		print(Rstar, Xstar_a, Ystar_a,myu_check)
+
 		
-		Rstar,Xstar_a,Ystar_a = self.get_R(60,2)
 
-		b1 = -math.tan(myu-fai)
-		c1 = Xstar_a * math.tan(myu - fai) - Ystar_a
-		b2 = -math.tan(-fai)
-
-		delta3 = b2 - b1
-
-		f = delta3 + math.tan(-fai)
-		g = delta3 - b2
-
-		print(b1,c1,b2,delta3,f,g)
-
-		Xstar_b = ((b1 * c1) / (f * g) + (c1 / f)) / (1.0 + (b2 * math.tan(-fai)) / (f * g))
-		Ystar_b = ((b1 * c1) - b2 * Xstar_b * math.tan(-fai)) / g
-
-		print(Xstar_b,Ystar_b)
+#		print(Rustar, math.degrees(theta_init))
 
 	# defines lower concave arc coordinates
 	# Precaution. there are still calculation uncertainties.
-	def new_concave(self,fai):
+	# ve is the entry angle
+	def lower_concave(self,v1,ve):
 		
-		v1 = 0
-		ve = 29
+		xinit = -math.sin(math.radians(v1))		
+		yinit= math.cos(math.radians(v1))		
 
-		fai = math.radians(fai)
-
-		xtmp = 0		#initial Xstar_b = 0
-		ytmp = 1		#initial Ystar_b = 1
+		xtmp = xinit
+		ytmp = yinit
 
 		x,y = [],[]
 		Xstar,Ystar = [],[]
 		R_x,R_y = [],[]
 
 		#v1 is the origin angle. ve is the end angle
-		for num in range(v1 + 1,ve):	
+		for num in range(v1,ve):	
 
 			Xstar_b = xtmp
 			Ystar_b = ytmp
-			
 			Rstar,Xstar_a,Ystar_a,myu_check = self.get_R(v1,num)
 			myu = self.get_myu(self.get_mach(self.get_Mstar(Rstar)))
-			a1 = math.tan(myu_check)
+			a1 = math.tan(-myu+math.radians(num))
 			b1 = Ystar_a - a1 * Xstar_a
 			a2 = math.tan(math.radians(num))
 			b2 = Ystar_b - a2 * Xstar_b
 
-#			print(Ystar_a,a1*Xstar_a)
-#			print(a1,b1,a2,b2,math.degrees(myu_check))
-#			print(Xstar_a,Ystar_a)
-
-#			print(Ystar_a)
-#			print(math.degrees(math.atan(a1)),b1,math.degrees(math.atan(a2)),b2)
-
 			xtmp = ((b2 - b1) / (a1 - a2))
 			ytmp = xtmp * a2 + b2
 
-#			print(Xstar_a,Ystar_a,math.degrees(myu_check))
-			print(xtmp,ytmp)
-			x += [float(xtmp)]
-			y += [float(ytmp)]
+			print(num,Rstar,math.degrees(myu_check),math.degrees(myu),xtmp,ytmp)
+			x += [(xtmp)]
+			y += [(ytmp)]
 			Xstar += [(Xstar_a)]
 			Ystar += [(Ystar_a)]
+
+			xcomp = np.arange(-1,0,0.001)
+			ycomp = a1*xcomp + b1
+
+			plt.plot(xcomp,ycomp)
+
+		for num in range(0,90):
 			R_x += [-math.sin(math.radians(num))]
 			R_y += [math.cos(math.radians(num))]
 
@@ -219,7 +254,9 @@ if __name__ == "__main__":
 	print("Design Supersonic Turbine")
 	f = Blade(1.4)
 	print("lower_concave")
-
-	f.new_concave(1)
+#	f.draw_lines(-10,20)
+#	f.get_R(-10,10)
+#	f.lower_concave(-8,30)
+	f.upper_convex(20,30)
 #	f.plot_chara(360,5)
 	print("finish")
